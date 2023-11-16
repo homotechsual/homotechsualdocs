2e8999690c784a0c3212d55910929436dca8c618
Mikey O'Toole <mikey@homotechsual.dev>
2023-11-16 17:40:16 +0000

    Update NinjaOne module docs.

--- src/theme/DocCard/index.js
+++ src/theme/DocCard/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import {
-  findFirstCategoryLink,
+  findFirstSidebarItemLink,
   useDocById,
 } from '@docusaurus/theme-common/internal';
 import isInternalUrl from '@docusaurus/isInternalUrl';
 import {translate} from '@docusaurus/Translate';
+import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileAlt, faCabinetFiling, faPaperclip } from '@fortawesome/sharp-solid-svg-icons';
@@ -22,9 +23,12 @@ function CardContainer({href, children}) {
 function CardLayout({href, icon, title, description}) {
   return (
     <CardContainer href={href}>
-      <h2 className={clsx('text--truncate', styles.cardTitle)} title={title}>
+      <Heading
+        as="h2"
+        className={clsx('text--truncate', styles.cardTitle)}
+        title={title}>
         {icon} {title}
-      </h2>
+      </Heading>
       {description && (
         <p
           className={clsx('text--truncate', styles.cardDescription)}
@@ -36,26 +40,29 @@ function CardLayout({href, icon, title, description}) {
   );
 }
 function CardCategory({item}) {
-  const href = findFirstCategoryLink(item);
+  const href = findFirstSidebarItemLink(item);
+  const icon = <FontAwesomeIcon icon={faCabinetFiling} />
   // Unexpected: categories that don't have a link have been filtered upfront
   if (!href) {
     return null;
   }
-  const icon = <FontAwesomeIcon icon={faCabinetFiling} />
   return (
     <CardLayout
       href={href}
       icon={icon}
       title={item.label}
-      description={translate(
-        {
-          message: '{count} items',
-          id: 'theme.docs.DocCard.categoryDescription',
-          description:
-            'The default description for a category card in the generated index about how many items this category includes',
-        },
-        {count: item.items.length},
-      )}
+      description={
+        item.description ??
+        translate(
+          {
+            message: '{count} items',
+            id: 'theme.docs.DocCard.categoryDescription',
+            description:
+              'The default description for a category card in the generated index about how many items this category includes',
+          },
+          {count: item.items.length},
+        )
+      }
     />
   );
 }
@@ -67,7 +74,7 @@ function CardLink({item}) {
       href={item.href}
       icon={icon}
       title={item.label}
-      description={doc?.description}
+      description={item.description ?? doc?.description}
     />
   );
 }
